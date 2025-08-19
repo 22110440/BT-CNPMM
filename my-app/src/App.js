e2bc9abf0c9971ce2e64a95d93eda5ebd0785040
22110440 <157661962+22110440@users.noreply.github.com>
2025-08-19 12:31:57 +0700

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div style={{ 
+      textAlign: "center", 
+      fontFamily: "Arial, sans-serif", 
+      marginTop: "50px" 
+    }}>
+      <img
+        src="/htt.jpg" alt="Ảnh cá nhân"
+        style={{width: "200px", borderRadius: "5%", marginBottom: "20px" }}
+      />
+      <h1>Hoàng Thanh Trí</h1>
+      <p>Tuổi: 21</p>
+      <p>MSSV: 22110440</p>
+    </div>
+  );
+}
+
+export default App;
