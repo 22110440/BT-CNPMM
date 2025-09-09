581c020bbf10bb6ebd8292949ae29f34d1314375
22110440 <157661962+22110440@users.noreply.github.com>
2025-09-09 19:21:01 +0700

    bổ sung

--- cnpmm_2708_client/src/App.jsx
+++ cnpmm_2708_client/src/App.jsx
@@ -30,14 +30,12 @@ function App() {
   return (
     <div>
       {appLoading === true ? (
-        <div
-          style={{
-            position: "fixed",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)"
-          }}
-        >
+        <div style={{
+          position: "fixed",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)"
+        }}>
           <Spin />
         </div>
       ) : (
