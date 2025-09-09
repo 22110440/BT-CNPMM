c2da33ba985dc8f54e09175f122c829ea80e8370
22110440 <157661962+22110440@users.noreply.github.com>
2025-09-09 22:17:56 +0700

    buoi 1

--- cnpmm_2008/src/controllers/homeController.js
+++ cnpmm_2008/src/controllers/homeController.js
@@ -1,75 +1,38 @@
-import db from '../../models/index.js';
-import CRUDService from '../services/CRUDService.js';
+const CRUDService = require('../services/CRUDService');
 
-let getHomePage = async (req, res) => {
-    try {
-        let data = await db.User.findAll();
-        return res.render('homepage.ejs', {
-            data: JSON.stringify(data)
-        });
-    } catch (e) {
-        console.log(e);
-    }
-}
+// Trang home (form tạo user)
+const getHome = (req, res) => {
+  res.render('home.ejs');
+};
 
-let getAboutPage = (req, res) => {
-    return res.render('test/about.ejs');
-}
+// Tạo user mới
+const postCreateUser = async (req, res) => {
+  await CRUDService.createUser(req.body);
+  res.redirect('/users');
+};
 
-let getCRUD = (req, res) => {
-    return res.render('crud.ejs');
-}
+// Lấy danh sách user
+const getUsers = async (req, res) => {
+  const users = await CRUDService.getAllUsers();
+  res.render('users/list.ejs', { users });
+};
 
-let getFindAllCrud = async (req, res) => {
-    let data = await CRUDService.getAllUser();
-    return res.render('users/findAllUser.ejs', {
-        datalist: data
-    });
-}
+// Lấy form edit user
+const getEditUser = async (req, res) => {
+  const user = await CRUDService.getUserById(req.params.id);
+  res.render('users/edit.ejs', { user });
+};
 
-let postCRUD = async (req, res) => {
-    let message = await CRUDService.createNewUser(req.body);
-    console.log(message);
-    return res.send('Post crud to server');
-}
+// Cập nhật user
+const postUpdateUser = async (req, res) => {
+  await CRUDService.updateUser(req.params.id, req.body);
+  res.redirect('/users');
+};
 
-let getEditCRUD = async (req, res) => {
-    let userId = req.query.id;
-    if (userId) {
-        let userData = await CRUDService.getUserInfoById(userId);
-        return res.render('users/editUser.ejs', {
-            data: userData
-        });
-    } else {
-        return res.send('không lấy được id');
-    }
-}
+// Xóa user
+const deleteUser = async (req, res) => {
+  await CRUDService.deleteUser(req.params.id);
+  res.redirect('/users');
+};
 
-let putCRUD = async (req, res) => {
-    let data = req.body;
-    let data1 = await CRUDService.updateUser(data);
-    return res.render('users/findAllUser.ejs', {
-        datalist: data1
-    });
-}
-
-let deleteCRUD = async (req, res) => {
-    let id = req.query.id;
-    if (id) {
-        await CRUDService.deleteUserById(id);
-        return res.send('Deleted!!!!!!!!!!!!');
-    } else {
-        return res.send('Not find user');
-    }
-}
-
-export default {
-    getHomePage:getHomePage,
-    getAboutPage:getAboutPage,
-    getCRUD:getCRUD,
-    postCRUD:postCRUD,
-    getFindAllCrud:getFindAllCrud,
-    getEditCRUD: getEditCRUD,
-    putCRUD: putCRUD,
-    deleteCRUD: deleteCRUD
-}
+module.exports = { getHome, postCreateUser, getUsers, getEditUser, postUpdateUser, deleteUser };
