32b60b24582c0d1b05292a52da6a731b9f84edbb
22110440 <157661962+22110440@users.noreply.github.com>
2025-09-10 09:19:02 +0700

    library 1009

--- Library_1009/frontend/src/index.js
+++ Library_1009/frontend/src/index.js
@@ -1,17 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Button from "./Button";
+import InputText from "./InputText";
+import Card from "./Card";
+import Modal from "./Modal";
+import Cart from "./Cart";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export { Button, InputText, Card, Modal, Cart };
